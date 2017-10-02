5c545f6158b6b2c51fa297d7cdd5fda91291ba73
Rafał Płaszczyk <rafal.plaszczyk@euvic.pl>
2017-10-02 23:55:48 +0200

    Lesson 20

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,6 +10,7 @@ var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
+var user_profile_component_1 = require("./users/user-profile.component");
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
@@ -19,7 +20,9 @@ var AppModule = /** @class */ (function () {
                 platform_browser_1.BrowserModule,
                 forms_1.FormsModule
             ],
-            declarations: [app_component_1.AppComponent
+            declarations: [
+                app_component_1.AppComponent,
+                user_profile_component_1.UserProfileComponent
             ],
             bootstrap: [app_component_1.AppComponent]
         })
