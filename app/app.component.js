993c6cf58c333395a2c9807c0ae513b15f75119a
Rafał Płaszczyk <rafal.plaszczyk@euvic.pl>
2017-10-02 05:32:43 +0200

    Lessons 9, 10, 11

---
+++ app/app.component.js
@@ -0,0 +1,31 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.message = 'Hello!';
+        this.users = [
+            { id: 25,
+                name: 'Rafal',
+                username: 'RAFIRAF' },
+            { id: 26, name: 'Nick', username: 'whatnicktweets' },
+            { id: 27, name: 'Holly', username: 'hollylawly' }
+        ];
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n    <header>\n      <nav class=\"navbar navbar-inverse\">\n        <div class=\"navbar-header\">\n          <a href=\"/\" class=\"navbar-brand\">My Angular App</a>\n        </div>\n      </nav>\n    </header>\n\n\n    <main>\n      <div class=\"jumbotron\">\n        <h1>Welcome to Our App!</h1>\n        <p>{{message}}</p>\n      </div>\n\n      <div *ngIf=\"users\">\n        <div *ngFor=\"let user of users\">\n          {{user.name}} ({{user.username}})\n        </div>\n      </div>\n    </main>\n\n    <footer class=\"text-center\">\n      Copyright &copy; 2017\n    </footer>\n  ",
+            styles: ["\n    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n  "]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
---
+++ app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <header>
+      <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+          <a href="/" class="navbar-brand">My Angular App</a>
+        </div>
+      </nav>
+    </header>
+
+
+    <main>
+      <div class="jumbotron">
+        <h1>Welcome to Our App!</h1>
+        <p>{{message}}</p>
+      </div>
+
+      <div *ngIf="users">
+        <div *ngFor="let user of users">
+          {{user.name}} ({{user.username}})
+        </div>
+      </div>
+    </main>
+
+    <footer class="text-center">
+      Copyright &copy; 2017
+    </footer>
+  `,
+  styles: [`
+    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
+  `]
+})
+export class AppComponent {
+  message = 'Hello!';
+  users = [
+    { id: 25,
+      name: 'Rafal',
+      username: 'RAFIRAF'},
+    { id: 26, name: 'Nick', username: 'whatnicktweets'},
+    {id: 27, name: 'Holly', username: 'hollylawly'}
+  ]
+}
