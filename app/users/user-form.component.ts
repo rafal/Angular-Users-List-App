57176e153a4e8a36f974b8509af15e81b9ee6670
Rafał Płaszczyk <rafal.plaszczyk@euvic.pl>
2017-10-03 18:57:20 +0200

    Lesson 23

--- app/users/user-form.component.ts
+++ app/users/user-form.component.ts
@@ -16,15 +16,19 @@ import { User } from '../shared/models/user';
     <form #form="ngForm">
     {{ form.valid }}
 
-      <div class="form-group">
+      <div class="form-group" [ngClass]="{ 'has-error': name.invalid && name.touched }">
         <input type="text" class="form-control"
-          name="name" required
+          name="name" placeholder="Name" required
           [(ngModel)]="newUser.name" #name="ngModel">
+
+        <span class="help-block" *ngIf="name.invalid && name.touched">Name is required.</span>
       </div>
 
-      <div class="form-group">
-        <input type="text" class="form-control" name="username" required
+      <div class="form-group" [ngClass]="{ 'has-error': username.invalid && username.touched }">
+        <input type="text" class="form-control" name="username" placeholder="Username" required
         [(ngModel)]="newUser.username" #username="ngModel">
+
+        <span class="help-block" *ngIf="username.invalid && username.touched">Username is required.</span>
       </div>
 
       <button type="submit" class="btn btn-lg btn-block btn-primary" [disabled]="form.invalid">
